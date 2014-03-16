c5b0f57393328ef04e673de79a8f863bfa197bc1
Yuri Pourre <yuriamayuri@gmail.com>
2014-03-16 16:44:03 -0300

    Adds Helper Method to Write/Read uniform objects

--- src/EEPROMAnything.h
+++ src/EEPROMAnything.h
@@ -1,6 +1,38 @@
 #include <EEPROM.h>
 #include <Arduino.h>  // for type definitions
 
+template <class T> int EEPROM_writeUniform(int block, const T& value)
+{
+    const byte* p = (const byte*)(const void*)&value;
+
+    unsigned int size = sizeof(value);
+    
+    unsigned int index = size*block;
+
+    unsigned int i;
+    
+    for (i = index; i < index+size; i++)
+          EEPROM.write(ee++, *p++);
+
+    return i;
+}
+
+template <class T> int EEPROM_readUniform(int ee, T& value)
+{
+    byte* p = (byte*)(void*)&value;
+
+    unsigned int size = sizeof(value);
+    
+    unsigned int index = size*block;
+
+    unsigned int i;
+
+    for (i = index; i < index+size; i++)
+          *p++ = EEPROM.read(ee++);
+
+    return i;
+}
+
 template <class T> int EEPROM_writeAnything(int ee, const T& value)
 {
     const byte* p = (const byte*)(const void*)&value;
